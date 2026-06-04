5d59a9c8e998cdf766826e501461f5415c96e4a9
TheLazyCat00 <manuel.stieger@proton.me>
2026-06-04 18:28:15 +0200

    improve naming

--- lib/cst/to_tree_graph.ml
+++ lib/cst/to_tree_graph.ml
@@ -2,12 +2,12 @@ let rec type_to_node = function
   | Nodes.Simple s -> Tree_graph.Leaf s
   | Nodes.FunctionType { params; ret_type } ->
       Tree_graph.Fields (Tree_graph.StringMap.of_list [
-        ("params", Tree_graph.Sequence (List.map type_to_node params));
+        ("param", Tree_graph.Sequence (List.map type_to_node params));
         ("type", type_to_node ret_type);
       ])
   | Nodes.MethodType { params; ret_type; is_mut } ->
       Tree_graph.Fields (Tree_graph.StringMap.of_list [
-        ("params", Tree_graph.Sequence (List.map type_to_node params));
+        ("param", Tree_graph.Sequence (List.map type_to_node params));
         ("type", type_to_node ret_type);
         ("is_mut", Tree_graph.Leaf (string_of_bool is_mut));
       ])
@@ -45,7 +45,7 @@ and statement_to_node (x: Nodes.statement) = match x with
         body = Tree_graph.Fields (
           Tree_graph.StringMap.of_list [
             ("callee", expr_to_node x.callee);
-            ("args", Tree_graph.Sequence (List.map expr_to_node x.args));
+            ("arg", Tree_graph.Sequence (List.map expr_to_node x.args));
           ]
         )
       }
@@ -54,7 +54,10 @@ and body_to_node (x: Nodes.body) = match x with
   | Nodes.Scope scope ->
       Tree_graph.Group {
         title = "scope";
-        body = Tree_graph.Sequence (List.map statement_to_node scope)
+        body = Tree_graph.Group {
+          title = "statement";
+          body = Tree_graph.Sequence (List.map statement_to_node scope)
+        }
       }
 
 and decl_to_node = function
@@ -63,7 +66,7 @@ and decl_to_node = function
         title = "function_decl";
         body = Tree_graph.Fields (
           Tree_graph.StringMap.of_list [
-            ("params", params_to_node params);
+            ("param", params_to_node params);
             ("ret_type", type_to_node ret_type);
             ("body", body_to_node body);
           ]
